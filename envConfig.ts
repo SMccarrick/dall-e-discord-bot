3fadb7175c33ea7508fe9c8fcf11d1dcc9cfe601
SMccarrick <smccarrick2804@gmail.com>
2023-12-24 00:29:22 +0000

    remove un-used env vars

--- envConfig.ts
+++ envConfig.ts
@@ -1,18 +1,14 @@
 import * as z from "zod";
 
 const EnvSchema = z.object({
-	DISCORD_SECRET: z.string(),
-	DISCORD_APPLICATION_ID: z.string(),
-	DISCORD_PUBLIC_KEY: z.string(),
+	DISCORD_BOT_SECRET: z.string(),
 	OPEN_AI_API_SECRET: z.string(),
 });
 
 type EnvType = z.infer<typeof EnvSchema>;
 
 export const envConfig: EnvType = {
-	DISCORD_SECRET: process.env.DISCORD_SECRET || "",
-	DISCORD_APPLICATION_ID: process.env.DISCORD_APPLICATION_ID || "",
-	DISCORD_PUBLIC_KEY: process.env.DISCORD_PUBLIC_KEY || "",
+	DISCORD_BOT_SECRET: process.env.DISCORD_BOT_SECRET || "",
 	OPEN_AI_API_SECRET: process.env.OPEN_AI_API_SECRET || "",
 };
 
