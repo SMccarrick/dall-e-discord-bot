f4919aa436eb08d4020dc034df0ffc25dd8ab704
SMccarrick <smccarrick2804@gmail.com>
2023-12-24 01:12:56 +0000

    add intents

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client } from "discord.js";
+import { Client, GatewayIntentBits, IntentsBitField } from "discord.js";
 import { envConfig } from "../envConfig";
 import { ready } from "./listeners/ready";
 import { interactionCreate } from "./listeners/interactionCreate";
@@ -6,7 +6,7 @@ import { interactionCreate } from "./listeners/interactionCreate";
 console.info("Bot is starting...");
 
 const client = new Client({
-	intents: [],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
 client.login(envConfig.DISCORD_BOT_SECRET);
