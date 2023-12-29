2aad218e018dd5bf1818cd21b0fd8cb7bbc42a75
SMccarrick <smccarrick2804@gmail.com>
2023-12-29 20:43:32 +0000

    remove un-used import

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client, GatewayIntentBits, IntentsBitField } from "discord.js";
+import { Client, GatewayIntentBits } from "discord.js";
 import { envConfig } from "../envConfig";
 import { ready } from "./listeners/ready";
 import { interactionCreate } from "./listeners/interactionCreate";
